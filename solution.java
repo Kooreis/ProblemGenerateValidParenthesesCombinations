f793a0e696d4a8143afc1e86ffddc504fa94caaa
Kooreis <kooreis.official@gmail.com>
2024-03-29 03:09:28 -0500

    In this update, the generateParenthesis method is added. It initializes an empty list and calls the backtrack method to fill the list with all valid combinations of n pairs of parentheses.

--- solution.java
+++ solution.java
@@ -1,11 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of pairs of parentheses:");
         int n = scanner.nextInt();
+        List<String> combinations = generateParenthesis(n);
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        backtrack(list, "", 0, 0, n);
+        return list;
     }
 }
