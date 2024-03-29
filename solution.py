5e310bf649bbbe6e180cdd409568c52805d2e766
Kooreis <kooreis.official@gmail.com>
2024-03-29 03:09:28 -0500

    This update adds a main function that prompts the user to enter the number of pairs of parentheses and then prints all valid combinations.

--- solution.py
+++ solution.py
@@ -1,7 +1,3 @@
-def generate_parentheses(n):
-    def generate(p, left, right, parens=[]):
-        if left:         generate(p + '(', left-1, right)
-        if right > left: generate(p + ')', left, right-1)
-        if not right:    parens.append(p)
-        return parens
-    return generate('', n, n)
+def main():
+    n = int(input("Enter the number of pairs of parentheses: "))
+    print(generate_parentheses(n))
